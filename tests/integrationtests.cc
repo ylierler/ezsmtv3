e93d1ab2348962baa5daac04b1755b86903010ac
Dhakalkeeran <keerandhakal@gmail.com>
2025-02-26 15:38:45 -0600

    Add unit test cases for domain and sum terms

--- tests/integrationtests.cc
+++ tests/integrationtests.cc
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <string.h>
 #include "catch2/catch_test_macros.hpp"
+#include "../src/read.h"
+#include "../src/logics/QF_LIA_logic.h"
 
 using std::string;
 using namespace std;
@@ -16,7 +18,7 @@ void assertEnumeratedAnswerSetsMatchExpected(string testName) {
     string outputPath = "temp.output";
 
     std::stringstream command;
-    command << "./ezsmt --solver-command \"../tools/cvc5 --lang smt --output-lang smt --incremental --seed 42\" " << inputPath << " -e -v 0 > " << outputPath;
+    command << "./ezsmt --solver-command \"../tools/cvc5 --lang smt --output-lang smt --incremental --seed 42\" " << inputPath << " -e -E -v 0 > " << outputPath;
 
     cout << "Running: " << command.str() << endl;
     int exitCode = system(command.str().c_str());
@@ -31,7 +33,7 @@ void assertEnumeratedAnswerSetsMatchExpected(string testName) {
     REQUIRE(exitCode == SUCCESS);
 }
 
-TEST_CASE("ezsmtplus enumerates all answer sets", "[integration]") {
+TEST_CASE("ezsmt3 enumerates all answer sets", "[integration]") {
     SECTION("n-queens") {
         assertEnumeratedAnswerSetsMatchExpected("n-queens");
     }
@@ -46,5 +48,26 @@ TEST_CASE("ezsmtplus enumerates all answer sets", "[integration]") {
     }
     SECTION("non-tight program should return empty answer set") {
         assertEnumeratedAnswerSetsMatchExpected("non-tight");
+    }
+}
+
+TEST_CASE("Checks domain term outputs"){
+    SECTION("domain term test 1") {
+        assertEnumeratedAnswerSetsMatchExpected("dom-test-1");
+    }
+    SECTION("domain term test 2") {
+        assertEnumeratedAnswerSetsMatchExpected("dom-test-2");
+    }
+    SECTION("domain term test 3") {
+        assertEnumeratedAnswerSetsMatchExpected("dom-test-3");
+    }
+}
+
+TEST_CASE("Checks sum term outputs"){
+    SECTION("sum term test 1") {
+        assertEnumeratedAnswerSetsMatchExpected("sum-test-1");
+    }
+    SECTION("sum term test 2") {
+        assertEnumeratedAnswerSetsMatchExpected("sum-test-2");
     }
 }
